15921561571d8ac214a29f29e642f7bb4635fd9b
lvoynov <berezawoodpanel@gmail.com>
2020-02-20 10:52:19 +0300

    Day_2

--- script/script.js
+++ script/script.js
@@ -20,7 +20,8 @@ const startButton = document.querySelector('.start-button'),
       formCalculate = document.querySelector('.form-calculate'),
       endButton = document.querySelector('.end-button'),
       total = document.querySelector('.total'),
-      fastRange = document.querySelector('.fast-range');
+      fastRange = document.querySelector('.fast-range'),
+      totalPriceSum = document.querySelector('.total_price__sum');
 
 function showElem(elem) {
   elem.style.display = 'block';
@@ -30,26 +31,65 @@ function hideElem(elem) {
   elem.style.display = 'none';
 }
 
-function priceCalculation(e) {
-  
+function priceCalculation(elem) {
+  let result = 0,
+      index = 0,
+      options = [];
+
+  if(elem.name === 'whichSite') {
+    for (const item of formCalculate.elements) {
+      if(item.type === 'checkbox') {
+        item.checked = false;
+      }
+    }
+    hideElem(fastRange);
+  }
+
+  for (const item of formCalculate.elements){
+    if(item.name === 'whichSite' && item.checked) {
+      index = DATA.whichSite.indexOf(item.value);
+    }else if(item.classList.contains('calc-handler') && item.checked) {
+      options.push(item.value);
+    }
+  }
+
+  options.forEach(function(key) {
+    if(typeof(DATA[key]) === 'number') {
+      if(key === 'sendOrder') {
+        result += DATA[key];
+      }else {
+        result += DATA.price[index] * DATA[key] / 100;
+      }
+    }else {
+      if(key === 'desktopTemplates') {
+        result += DATA.price[index] * DATA.desktopTemplates[index] / 100;
+      }else {
+        result += DATA[key][index];
+      }
+    }
+  });
+
+
+  result += DATA.price[index];
+
+  totalPriceSum.textContent = result;
 }
 
 function handlerCallBackForm(e) {
   const target = e.target;
 
   if(target.classList.contains('want-faster')) {
-    if(target.checked){
-      showElem(fastRange);
-    } else {
-      hideElem(fastRange);
-    }
-    
-    //  target.checked ? showElem(fastRange) : hideElem(fastRange);
-    if(target.classList.contains('calc-handler')) {
-      priceCalculation(target)
-    }
+     target.checked ? showElem(fastRange) : hideElem(fastRange);
+    // if(target.checked){
+    //   showElem(fastRange);
+    // } else {
+    //   hideElem(fastRange);
+    // }
   }
-console.log(e.target);
+ if(target.classList.contains('calc-handler')) {
+      priceCalculation(target);
+    }
+
 }
 
 startButton.addEventListener('click', function() {
